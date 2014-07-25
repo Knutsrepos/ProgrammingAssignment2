a9df3820dcf6e8df7c87ed25a6fd92ae6f262c49
Knutsrepos <mail_an_tobi@gmx.de>
2014-07-25 12:24:53 +0200

    Calculation of the Inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,38 @@
 ## functions do
 
 ## Write a short comment describing this function
+##This function basically works like the example functions makeVector(). The only difference
+##is that we now have a matrix instead of a vector
+##Here, setinverse sets the inverse of the matrix and getinverse gets the inverse 
+##of a matrix from the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+## The function cacheSolve is also very similar to the function cachemean.
+## Calculating the mean of a vector it calculates the inverse of a matrix via 
+## the function solve()
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
